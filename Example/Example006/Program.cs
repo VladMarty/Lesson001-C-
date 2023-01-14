c933ae6e2c1b08daf8a61dd34853406454d519b1
Vlad <efimovich94@inbox.ru>
2023-01-14 16:36:44 +0300

    Example006

---
+++ Example/Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "айзада")
+{
+    Console.WriteLine("Ура! Это же Айзада!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
